87aee99b597cc32626176a6859792a66fe0f7b83
kian-darbedar <kiandarbandi24@gmail.com>
2021-02-28 14:25:18 +0330

    product

---
+++ product/forms.py
@@ -0,0 +1,8 @@
+from django import  forms
+from .models import Product
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['title', ' brand', 'seller', 'price', 'image', 'colors', 'serial', 'body']
--- product/views.py
+++ product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .forms import ProductForm
+from .models import Product
 # Create your views here.
+
+def product_create(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
